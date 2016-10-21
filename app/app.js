a3b5b468a8d4c7e402176d0e914540aa8831d2a7
Shivangi Rai <shrai@adobe.com>
2016-10-21 15:58:23 +0530

    commint 2

--- app/app.js
+++ app/app.js
@@ -1,9 +1,38 @@
+var translationsEN = {
+  HEADLINE: 'Welcome to...!!' ,
+  NAME: 'Name' ,
+  EMAIL: 'E-mail' ,
+  BUTTON_LANG_DE : 'German' , 
+  BUTTON_LANG_EN : 'English' ,
+  ZoomIn : 'Proceed',
+  cancel : 'Cancel' ,
+  hello : 'Hello' ,
+  errorMsg : 'Please enter Name'
+} ;
 
-var app = angular.module('myApp' , []);
+var translationsDE = {
+   HEADLINE: 'Willkommen zu...!!' ,  
+   NAME: 'Name',
+   BUTTON_LANG_DE : 'Deutsche' , 
+   BUTTON_LANG_EN : 'Englisch' ,
+   ZoomIn : 'Vorgehen',
+   cancel : 'Stornieren' ,
+   hello : 'Hallo' ,
+   errorMsg : 'Bitte geben Sie den Namen'
+}
+var app = angular.module('myApp' , ['pascalprecht.translate']);
+
+app.config(['$translateProvider', function ($translateProvider) {
+  $translateProvider.translations('en', translationsEN);
+  $translateProvider.translations('de', translationsDE);
+  $translateProvider.determinePreferredLanguage();
+  $translateProvider.fallbackLanguage('en');
+}]);
 
 app.controller('Ctrl', ['$scope', function ( $scope) {
  
   $scope.changeLanguage = function (langKey) {
+    $translate.use(langKey);
   };
  
   $scope.validate = function(){
