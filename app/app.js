8c1a8dd70e25ed743ef069784df40fa2e45e3981
Shivangi Rai <shrai@adobe.com>
2016-10-27 15:01:35 +0530

    First presentation changes

--- app/app.js
+++ app/app.js
@@ -1,55 +1,17 @@
-var translationsEN = {
-  HEADLINE: 'Welcome to...!!' ,
-  NAME: 'Name' ,
-  EMAIL: 'E-mail' ,
-  BUTTON_LANG_DE : 'German' , 
-  BUTTON_LANG_EN : 'English' ,
-  ZoomIn : 'Proceed',
-  cancel : 'Cancel' ,
-  hello : 'Hello' ,
-  errorMsg : 'Please enter Name'
-} ;
 
-var translationsDE = {
-   HEADLINE: 'Willkommen zu...!!' ,  
-   NAME: 'Name',
-   BUTTON_LANG_DE : 'Deutsche' , 
-   BUTTON_LANG_EN : 'Englisch' ,
-   ZoomIn : 'Vorgehen',
-   cancel : 'Stornieren' ,
-   hello : 'Hallo' ,
-   errorMsg : 'Bitte geben Sie den Namen'
-}
+var app = angular.module('myApp' , []);
 
-
-
-
-var app = angular.module('myApp', ['pascalprecht.translate']);
-app.controller('Ctrl', ['$scope', function ($scope) {
- }]);
-
- 
-app.config(['$translateProvider', function ($translateProvider) {
-  $translateProvider.translations('en', translationsEN);
-  $translateProvider.translations('de', translationsDE);
-  $translateProvider.determinePreferredLanguage();
-  $translateProvider.fallbackLanguage('en');
-}]);
- 
-app.controller('Ctrl', ['$translate', '$scope', '$filter', function ($translate, $scope, $filter) {
- 
+app.controller('Ctrl', ['$scope', function ( $scope) {
  
   $scope.changeLanguage = function (langKey) {
-    $translate.use(langKey);
   };
-  
+ 
   $scope.validate = function(){
 	  if(typeof $scope.name == 'undefined' || $scope.name == '' ){
-		  alert($filter('translate')('errorMsg')) ;
+		  alert('Please enter name !!') ;
 		  return ;
 	  }
-		var hello = $filter('translate')('hello') ; 
-		alert( $filter('translate')('hello') + ' ' + $scope.name) ;
+	  alert('Hello '+ $scope.name ) ;
   }
   
   $scope.cancel= function(){
