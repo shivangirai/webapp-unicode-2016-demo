ed587a49e570a6fb7103f284a5152b23dac50e25
Shivangi Rai <shrai@adobe.com>
2016-10-27 15:20:20 +0530

    second Presentation Changes

--- app/app.js
+++ app/app.js
@@ -1,9 +1,40 @@
+var translationsEN = {
+  HEADLINE: 'Welcome to...!!' ,
+  NAME: 'Name' ,
+  EMAIL: 'E-mail' ,
+  BUTTON_LANG_DE : 'German' , 
+  BUTTON_LANG_EN : 'English' ,
+  ZoomIn : 'Proceed',
+  cancel : 'Cancel' ,
+  hello : 'Hello' ,
+  errorMsg : 'Please enter Name'
+} ;
 
-var app = angular.module('myApp' , []);
+var translationsDE = {
+   HEADLINE: 'Willkommen zu...!!' ,  
+   NAME: 'Name',
+   BUTTON_LANG_DE : 'Deutsche' , 
+   BUTTON_LANG_EN : 'Englisch' ,
+   ZoomIn : 'Vorgehen',
+   hello : 'Hallo' ,
+   errorMsg : 'Bitte geben Sie den Namen'
+} ;
 
-app.controller('Ctrl', ['$scope', function ( $scope) {
+
+var app = angular.module('myApp' , ['pascalprecht.translate']);// Introducing angular translate dependancy
+
+
+app.config(['$translateProvider', function ($translateProvider) {
+  $translateProvider.translations('en', translationsEN);
+  $translateProvider.translations('de', translationsDE);
+  $translateProvider.determinePreferredLanguage();
+  $translateProvider.fallbackLanguage('en');
+}]);// Config Addition
+
+app.controller('Ctrl', ['$translate', '$scope', function ( $translate ,  $scope  ) {
  
-  $scope.changeLanguage = function (langKey) {
+   $scope.changeLanguage = function (langKey) {
+    $translate.use(langKey);
   };
  
   $scope.validate = function(){
